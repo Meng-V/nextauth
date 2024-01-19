2b932aa8ac2c89c36419ed6afa02e2a1cc9626c3
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2024-01-18 22:59:32 -0500

    added shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <p className="font-semibold">Hello Auth!</p>;
+  return (
+    <Button>
+      Click Me
+    </Button>
+  );
 }
