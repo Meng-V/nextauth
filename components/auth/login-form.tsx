7e6f56f8c11ac0959de216182edc36e0f6c1e78e
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2024-01-21 00:03:51 -0500

    update login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,6 +5,8 @@ import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LoginSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 import {
   Form,
@@ -34,9 +36,42 @@ export const LoginForm = () => {
     >
       <Form {...form}>
         <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
-        <div className="space-y-4">
-        <FormField />
-        </div>
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="john.doe@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
         </form>
       </Form>
     </CardWrapper>
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,8 +1,11 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string()
-    // password: z.string().min(6)
+  email: z.string().email({
+    message: "You have to give a valid email address.",
+  }),
+  password: z.string(),
+  // password: z.string().min(6, {
+  //     message: "Password is required"
+  // })
 });
-
