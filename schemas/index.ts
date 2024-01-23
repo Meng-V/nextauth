ea93825ab672738835d8496c1fd1e7470ab54fbc
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2024-01-22 22:37:02 -0500

    edit as register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,8 +4,19 @@ export const LoginSchema = z.object({
   email: z.string().email({
     message: "You have to give a valid email address.",
   }),
-  password: z.string(),
-  // password: z.string().min(6, {
-  //     message: "Password is required"
-  // })
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "You have to give a valid email address.",
+  }),
+  password: z.string().min(2, {
+    message: "Minimum 2 characters required",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
 });
