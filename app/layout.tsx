319002758b0ebd4afd64d5f8d7a84e138f3428bc
Meng-V <qum@miamioh.edu>
2024-02-09 21:02:31 -0500

    prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,8 +18,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
