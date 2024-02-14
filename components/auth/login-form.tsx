b29b7b310ebab89de3951d541beb9c1c20e90b5b
Meng-V <qum@miamioh.edu>
2024-02-14 17:07:24 -0500

    Update login-form.tsx

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -74,7 +74,7 @@ export const LoginForm = () => {
                     <Input
                       {...field}
                       disabled={isPending}
-                      placeholder="john.doe@example.com"
+                      placeholder="john.doe@example.com "
                       type="email"
                     />
                   </FormControl>
