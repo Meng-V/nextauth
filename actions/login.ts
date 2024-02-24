f2d67e70296a2bf44e4e985411462289759ef06e
Meng-V <qum@miamioh.edu>
2024-02-24 17:19:02 -0500

    added callbacks

--- actions/login.ts
+++ actions/login.ts
@@ -25,7 +25,7 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     if (error instanceof AuthError) {
       switch (error.type) {
         case "CredentialsSignin":
-          return { error: "Invalid credentials!" };
+          return { error: "Wrong credentials!" };
         default:
           return { error: "Something went wrong!" };
       }
